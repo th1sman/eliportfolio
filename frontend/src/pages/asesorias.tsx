f12d4bdcf6d3b248a4cbb7c61169b02f960e01da
th1sman <jaim.quilodrandev@gmail.com>
2024-01-11 20:59:18 -0300

    mobile dessing missing

--- frontend/src/pages/asesorias.tsx
+++ frontend/src/pages/asesorias.tsx
@@ -1,14 +1,36 @@
 import React from "react";
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
+import styles from "@/styles/asesorias.module.css";
+import Image from "next/image";
 
 export default function Asesorias() {
   return (
     <div>
       <Navigation />
-      <h1>Asesorias</h1>
+      <section>
+        <h2 className={styles.title}>Podemos asesorarte</h2>
+        <p className={styles.title}>
+          Asesoramos a pequeños, medianos y grandes negocios relacionados con el
+          rubro de turismo y gastronomia y también a personas que quieran
+          iniciar en el mundo gastronómico y que busquen invertir de forma mas
+          segura encaminando hacia el exito su proyecto. Tambien a los negocios
+          que quieran potenciar sus propuestas y rentabilizar de mejor manera su
+          empresa
+        </p>
+        <div className={styles.consulting}>
+          <div className={styles.box}>
+            <Image src="/12.webp" alt="Image1" fill sizes="100vw" />
+          </div>
+          <div className={styles.box}>
+            <Image src="/12.webp" alt="Image1" fill />
+          </div>
+          <div className={styles.box}>
+            <Image src="/12.webp" alt="Image1" fill />
+          </div>
+        </div>
+      </section>
       <Footer />
     </div>
   );
 }
-
--- frontend/src/pages/cenas.tsx
+++ frontend/src/pages/cenas.tsx
@@ -1,12 +1,59 @@
-import React from 'react'
-import Navigation from '@/components/Navigation'
+import React from "react";
+import styles from "@/styles/dinners.module.css";
+import Navigation from "@/components/Navigation";
+import Image from "next/image";
 
-
-export default function Cenas () {
-    return (
-        <div>
-            <Navigation />
-            <h1>Cenas</h1>
+export default function Cenas() {
+  return (
+    <div>
+      <Navigation />
+      <div className={styles.container}>
+        <div className={styles.item}>
+          <div className={styles.imageBox}>
+            <Image src="/12.webp" alt="Image1" fill sizes="100vw" />
+          </div>
         </div>
-    )
+        <div className={styles.item}>
+          <div className={styles.imageBox}>
+            <Image src="/12.webp" alt="Image1" fill sizes="100vw" />
+          </div>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.imageBox}>
+            <Image src="/12.webp" alt="Image1" fill sizes="100vw" />
+          </div>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.imageBox}>
+            <Image src="/12.webp" alt="Image1" fill sizes="100vw" />
+          </div>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.imageBox}>
+            <Image src="/12.webp" alt="Image1" fill sizes="100vw" />
+          </div>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.imageBox}>
+            <Image src="/12.webp" alt="Image1" fill sizes="100vw" />
+          </div>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.imageBox}>
+            <Image src="/12.webp" alt="Image1" fill sizes="100vw" />
+          </div>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.imageBox}>
+            <Image src="/12.webp" alt="Image1" fill sizes="100vw" />
+          </div>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.imageBox}>
+            <Image src="/12.webp" alt="Image1" fill sizes="100vw" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
